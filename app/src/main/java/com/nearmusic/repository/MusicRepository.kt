2a3779a923ad7c7d842031f61f3095aaa25b84ee
k1405 <63481511+k1405@users.noreply.github.com>
2022-12-14 18:58:59 -0600

    notes sobre el repository

--- app/src/main/java/com/nearmusic/repository/MusicRepository.kt
+++ app/src/main/java/com/nearmusic/repository/MusicRepository.kt
@@ -3,7 +3,7 @@ package com.nearmusic.repository
 import androidx.lifecycle.MutableLiveData
 import com.nearmusic.data.MusicDao
 import com.nearmusic.model.Music
-
+// aca se genero el music repository  donde se utiliza musica dao y las funciones save and delete
 class MusicRepository(private val musicDao: MusicDao) {
 
    fun saveMusic(music: Music) {
